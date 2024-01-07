f65206131e7c958bffe87df53324c3effec2d4ff
Kredwi <kredwi888@gmail.com>
2024-01-07 22:41:27 +0900

    Update unban.js
    Delete comments

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -1,51 +1,51 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js'); // Подключение библиотеки Discord.JS
-const getLocaleFile = require('../../locale.js'); // Подключение файла который отвечает за локализацию
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const getLocaleFile = require('../../locale.js');
 
-module.exports = { // экспорт модуля для других файлов
-    data: new SlashCommandBuilder() // Запуск класса SlashCommandBuilder
-    .setName('unban') // Название команды
-    .setNameLocalizations({ // Название команды в локализации
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('unban')
+    .setNameLocalizations({
         ru: 'разбан'
     })
-    .setDescription('Unban/Unblock the user') // Описание команды
-    .setDescriptionLocalizations({ // Описание команды в локализации
+    .setDescription('Unban/Unblock the user')
+    .setDescriptionLocalizations({
         ru: 'Разбанить/Разблокировать пользователя'
     })
-    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers) // Установка прав для вывода команды
-    .addStringOption(option => option.setName('member').setNameLocalizations({ // Пользователь должен ввести тут что-то лень писать
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+    .addStringOption(option => option.setName('member').setNameLocalizations({
         ru: 'участник'
-    }).setDescription('enter the member ID or alias (without numbers after # and without #)').setDescriptionLocalizations({ // локализация
+    }).setDescription('enter the member ID or alias (without numbers after # and without #)').setDescriptionLocalizations({
         ru: 'введите ID участника или псевдоним (без цифр после # и без #)'
     }).setRequired(true)),
-    async execute(interaction) { // Создание асинхронной функции
+    async execute(interaction) {
         try {
-            const lang = await getLocaleFile(interaction.guildLocale); // Вызов функции для получение файла локализации
-            const userTag = interaction.options.getString('member'); // Получение информации которую ввёл пользователь
-            const users = await interaction.guild.bans.fetch() // Получение списка банов
-            .then(async bans => { // Обработка банов
-                return bans.map(member => { // перебор массива
+            const lang = await getLocaleFile(interaction.guildLocale);
+            const userTag = interaction.options.getString('member');
+            const users = await interaction.guild.bans.fetch()
+            .then(async bans => {
+                return bans.map(member => {
                     while (
-                        member.user.username == userTag || // Если псевдоним будет равен тому что ввёл пользователь то тогда его разбанит
-                        member.user.id == userTag // Если id будет равен тому что ввёл пользователь то тогда его разбанит
-                    ) return member.user; // Возрашение списка пользователя в массив
+                        member.user.username == userTag ||
+                        member.user.id == userTag
+                    ) return member.user;
                 });
             });
-            const user = users.filter(user => user != undefined); // Удаление всех записей где пусто
-            if (!user[0]) return await interaction.reply({ content: userTag + lang.userNotBanned, ephemeral: true}); // Проверка есть ли такой пользователь, нету нету то отправляется сообщение что такого пользователя не найдено
-            interaction.guild.members.unban(user[0]); // Разбанивание пользователя
-            const embedUnban = new EmbedBuilder() // Создание embed-сообщение
-            .setColor(0xff0000) // Цвет карсный
-            .setAuthor({ // Автор кого разбанивают
+            const user = users.filter(user => user != undefined);
+            if (!user[0]) return await interaction.reply({ content: userTag + lang.userNotBanned, ephemeral: true});
+            interaction.guild.members.unban(user[0]);
+            const embedUnban = new EmbedBuilder()
+            .setColor(0xff0000)
+            .setAuthor({
                 name: user[0].discriminator > 0 ? user[0].username + '#' + user[0].discriminator : user[0].username,
                 iconURL: user[0].displayAvatarURL(),
             })
-            .setTitle(lang.userUnbanned) // Загловок embed-сообщение
-            .setDescription(lang.user + '<@' + user[0].id + '>') // Описание embed-сообщение
-            .setFooter({ text: lang.discordServer + interaction.guild.name }) // Футер embed-сообщение
-            .setTimestamp(); // Вывод даты создание сообщение
-            await interaction.reply({ embeds: [embedUnban], ephemeral: false }); // Отправка всех сообщений
-        } catch (error) { // Обработка ошибок
-            console.log(error); // Вывод ошибки
+            .setTitle(lang.userUnbanned)
+            .setDescription(lang.user + '<@' + user[0].id + '>')
+            .setFooter({ text: lang.discordServer + interaction.guild.name })
+            .setTimestamp();
+            await interaction.reply({ embeds: [embedUnban], ephemeral: false });
+        } catch (error) {
+            console.log(error);
         }
     }
 }
